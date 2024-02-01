512dbe825e3e9fa3019e28466f137cb6ff181129
D3lux3 <dani.haapaniemi@hotmail.com>
2024-02-01 18:49:55 +0200

    added block storage and typings

--- src/Board.ts
+++ src/Board.ts
@@ -1,14 +1,27 @@
+
+interface Block {
+  block: string,
+  location: {
+    x: number,
+    y: number
+  }
+}
+
 export class Board {
   width: number;
   height: number;
+  blocks: Block[];
 
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
+    this.blocks = [];
   }
 
   drop(block: string): void {
-
+    const boardMiddlePoint = Math.floor(this.width / 2);
+    const newBlock = { block, location: { x: boardMiddlePoint, y: 0 } };
+    this.blocks = [newBlock, ...this.blocks];
   }
 
   toString() {
