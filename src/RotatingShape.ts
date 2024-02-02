984334a8a18891cc1812a87a8e616c2d2237f299
D3lux3 <dani.haapaniemi@hotmail.com>
2024-02-02 19:03:04 +0200

    Rotating right added

--- src/RotatingShape.ts
+++ src/RotatingShape.ts
@@ -7,7 +7,15 @@ export class RotatingShape {
         this.shape = shape.split("\n").map((str) => str.trim() + "\n").join("");
     }
 
-    toString() {
+    rotateRight(): RotatingShape {
+        const lines = this.shape.split("\n").slice(0, -1)
+        const transpose = lines[0].split('').map((_, col) => {
+            return lines.map(line => line[col]).reverse().join('')
+        }).join("\n");
+        return new RotatingShape(transpose);
+    }
+
+    toString(): string {
         return this.shape;
     }
 }
