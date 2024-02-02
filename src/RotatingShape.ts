bc618ac298c0e016df4cda6469b534950ba70e58
D3lux3 <dani.haapaniemi@hotmail.com>
2024-02-02 17:40:44 +0200

    created basic skeleton for rotating shape

---
+++ src/RotatingShape.ts
@@ -0,0 +1,13 @@
+
+export class RotatingShape {
+
+    readonly shape: string
+
+    constructor(shape: string) {
+        this.shape = shape;
+    }
+
+    toString() {
+        return this.shape;
+    }
+}
