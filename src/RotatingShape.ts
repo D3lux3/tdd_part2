006ac06b79d5b4c3380ab604ab834de89d464a44
D3lux3 <dani.haapaniemi@hotmail.com>
2024-02-02 19:45:32 +0200

    Refactored rotating algo

--- src/RotatingShape.ts
+++ src/RotatingShape.ts
@@ -11,20 +11,20 @@ export class RotatingShape {
         return this.shape.split("\n");
     }
 
+    private rotate(lines: string[]): string[] {
+        return lines[0].split('').map((_, col) => lines.map(line => line[col]).join(''));
+    }
+
     rotateRight(): RotatingShape {
         const lines = this.shapeAsLines();
-        const transpose = lines[0].split('').map((_, col) => {
-            return lines.map(line => line[col]).reverse().join('')
-        }).join("\n");
-        return new RotatingShape(transpose);
+        const rotated = this.rotate(lines.reverse()).join("\n");
+        return new RotatingShape(rotated);
     }
 
     rotateLeft(): RotatingShape {
         const lines = this.shapeAsLines();
-        const transpose = lines[0].split('').map((_, col) => {
-            return lines.map(line => line.split('').reverse().join('')[col]).join('')
-        }).join("\n");
-        return new RotatingShape(transpose);
+        const rotated = this.rotate(lines).reverse().join("\n");
+        return new RotatingShape(rotated);
     }
 
     toString(): string {
