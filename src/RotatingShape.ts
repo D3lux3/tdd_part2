d60450c1212690a0292f86c0d52425c741b56b6f
D3lux3 <dani.haapaniemi@hotmail.com>
2024-02-02 17:45:26 +0200

    shape formatting to constructor

--- src/RotatingShape.ts
+++ src/RotatingShape.ts
@@ -4,7 +4,7 @@ export class RotatingShape {
     readonly shape: string
 
     constructor(shape: string) {
-        this.shape = shape;
+        this.shape = shape.split("\n").map((str) => str.trim() + "\n").join("");
     }
 
     toString() {
