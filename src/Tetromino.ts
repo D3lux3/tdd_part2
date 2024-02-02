9ffe56b7318d582e6b32a03fe8e2911462e66f8d
D3lux3 <dani.haapaniemi@hotmail.com>
2024-02-03 01:07:01 +0200

    I shape can rotate right

--- src/Tetromino.ts
+++ src/Tetromino.ts
@@ -2,26 +2,23 @@ import { RotatingShape } from "./RotatingShape";
 
 export class Tetromino {
 
-    static readonly T_SHAPE = new Tetromino(`.T.\nTTT\n...`);
-    static readonly I_SHAPE = new Tetromino(`.....\n.....\nIIII.\n.....\n.....`)
+    static readonly T_SHAPE = new Tetromino(4, `.T.\nTTT\n...`);
+    static readonly I_SHAPE = new Tetromino(2, `.....\n.....\nIIII.\n.....\n.....`)
 
-
+    readonly orientations: number;
     readonly shape: RotatingShape;
 
-    constructor(shape: string) {
+    constructor(orientations: number, shape: string) {
         this.shape = new RotatingShape(shape);
-    }
-
-    private rotate(lines: string[]): string[] {
-        return lines[0].split('').map((_, col) => lines.map(line => line[col]).join(''));
+        this.orientations = orientations;
     }
 
     rotateRight(): Tetromino {
-        return new Tetromino(this.shape.rotateRight().shape)
+        return new Tetromino(this.orientations, this.shape.rotateRight().shape)
     }
 
     rotateLeft(): Tetromino {
-        return new Tetromino(this.rotate(this.shape.shape.split("\n")).join("\n"))
+        return new Tetromino(this.orientations, this.shape.rotateLeft().shape);
     }
 
 
