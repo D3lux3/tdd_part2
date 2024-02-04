31ff6816f169371f9e13e817c470971573f6eed9
D3lux3 <dani.haapaniemi@hotmail.com>
2024-02-04 16:00:28 +0200

    Added orientations

--- src/Tetromino.ts
+++ src/Tetromino.ts
@@ -2,23 +2,36 @@ import { RotatingShape } from "./RotatingShape";
 
 export class Tetromino {
 
-    static readonly T_SHAPE = new Tetromino(4, `.T.\nTTT\n...`);
-    static readonly I_SHAPE = new Tetromino(2, `.....\n.....\nIIII.\n.....\n.....`)
+    static readonly T_SHAPE = new Tetromino(4, 0, `.T.\nTTT\n...`);
+    static readonly I_SHAPE = new Tetromino(2, 0, `.....\n.....\nIIII.\n.....\n.....`)
 
-    readonly orientations: number;
+    readonly orientations: RotatingShape[];
+
     readonly shape: RotatingShape;
+    readonly currentOrientation: number;
 
-    constructor(orientations: number, shape: string) {
+    constructor(maxOrientations: number, currentOrientation: number, shape: string) {
         this.shape = new RotatingShape(shape);
-        this.orientations = orientations;
+        this.orientations = this.createOrientations(1, [this.shape], maxOrientations);
+        this.currentOrientation = currentOrientation;
+    }
+
+    private createOrientations(i: number, shapes: RotatingShape[], maxOrientations: number): RotatingShape[] {
+        return i == maxOrientations ? shapes : this.createOrientations(i + 1, [...shapes, (shapes.pop() as RotatingShape).rotateRight()], maxOrientations)
+    }
+
+    private scaleOrientation(orientation: number) {
+        return (this.orientations.length + orientation) % this.orientations.length
     }
 
     rotateRight(): Tetromino {
-        return new Tetromino(this.orientations, this.shape.rotateRight().shape)
+        const scaled = this.scaleOrientation(this.currentOrientation + 1);
+        return new Tetromino(this.orientations.length, scaled, this.orientations[scaled].shape);
     }
 
     rotateLeft(): Tetromino {
-        return new Tetromino(this.orientations, this.shape.rotateLeft().shape);
+        const scaled = this.scaleOrientation(this.currentOrientation - 1);
+        return new Tetromino(this.orientations.length, scaled, this.orientations[scaled].shape);
     }
 
 
