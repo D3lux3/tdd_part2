6df6f435191cb2ee89c1650857ea64eab78fb235
D3lux3 <dani.haapaniemi@hotmail.com>
2024-02-04 22:00:02 +0200

    added O shape

--- src/Tetromino.ts
+++ src/Tetromino.ts
@@ -3,11 +3,11 @@ import { RotatingShape } from "./RotatingShape";
 export class Tetromino {
 
     static readonly T_SHAPE = new Tetromino(4, 0, `.T.\nTTT\n...`);
-    static readonly I_SHAPE = new Tetromino(2, 0, `.....\n.....\nIIII.\n.....\n.....`)
+    static readonly I_SHAPE = new Tetromino(2, 0, `.....\n.....\nIIII.\n.....\n.....`);
+    static readonly O_SHAPE = new Tetromino(1, 1, `.OO\n.OO\n...`);
 
     readonly orientations: RotatingShape[];
     readonly maxOrientations: number;
-
     readonly shape: RotatingShape;
     readonly currentOrientation: number;
 
@@ -19,7 +19,7 @@ export class Tetromino {
     }
 
     private createOrientations(i: number, shapes: RotatingShape[]): RotatingShape[] {
-        return i == this.maxOrientations ? shapes : this.createOrientations(i + 1, [...shapes, (shapes.pop() as RotatingShape).rotateRight()])
+        return (i >= this.maxOrientations || i > 4) ? shapes : this.createOrientations(i + 1, [...shapes, (shapes.pop() as RotatingShape).rotateRight()])
     }
 
     private scaleOrientation(orientation: number) {
