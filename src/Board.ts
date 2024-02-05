05a199d38737bfe60bd6a6688cd8093c87c1733b
D3lux3 <dani.haapaniemi@hotmail.com>
2024-02-05 20:21:47 +0200

    Removed Block type usage from Board

--- src/Board.ts
+++ src/Board.ts
@@ -10,7 +10,7 @@ interface Block {
 export class Board {
   width: number;
   height: number;
-  blocks: Map<string, Block>;
+  blocks: Map<string, Tetromino>;
   fallingBlockId: string | undefined
 
   constructor(width: number, height: number) {
@@ -20,19 +20,19 @@ export class Board {
   }
 
   private isSquareValid(x: number, y: number): boolean {
-    return y < this.height && [...this.blocks.values()].map((block) => block.location[0]).filter(({ x: blockX, y: blockY }) => blockX === x && blockY === y).length === 0;
+    return y < this.height && [...this.blocks.values()].map((block) => block.coordinates[0]).filter(({ x: blockX, y: blockY }) => blockX === x && blockY === y).length === 0;
   }
 
-  getFallingBlock(): Block | undefined {
+  getFallingBlock(): Tetromino | undefined {
     return this.fallingBlockId ? this.blocks.get(this.fallingBlockId) : undefined;
   }
 
   tick(): void {
     const fallingBlock = this.getFallingBlock();
     if (fallingBlock) {
-      const newYCoord = fallingBlock.location[0].y + 1;
-      if (this.isSquareValid(fallingBlock.location[0].x, newYCoord)) {
-        this.blocks.set((this.fallingBlockId as string), { ...fallingBlock, location: [{ x: fallingBlock.location[0].x, y: newYCoord }] })
+      const newYCoord = fallingBlock.coordinates[0].y + 1;
+      if (this.isSquareValid(fallingBlock.coordinates[0].x, newYCoord)) {
+        this.blocks.set((this.fallingBlockId as string), fallingBlock.setCoordinates([{ x: fallingBlock.coordinates[0].x, y: newYCoord }]))
         return;
       }
       delete this.fallingBlockId;
@@ -52,7 +52,7 @@ export class Board {
       const blockId = uuidv4();
       const startingCoordinates: Coordinate[] = [{ x: Math.floor(this.width / 2), y: 0 }]
       const converted = this.tetrominoConverter(block).setCoordinates(startingCoordinates);
-      this.blocks.set(blockId, { block: converted, location: startingCoordinates })
+      this.blocks.set(blockId, converted)
       this.fallingBlockId = blockId;
       return;
     }
@@ -62,9 +62,9 @@ export class Board {
   toString() {
     const emptyBoard = (".".repeat(this.width) + "\n").repeat(this.height);
     return [...this.blocks.values()].reduce((b, block) => {
-      const { x, y } = block.location[0];
+      const { x, y } = block.coordinates[0];
       const blockIndexOnBoard = (y * (this.width + 1)) + x;
-      return b.substring(0, blockIndexOnBoard) + `${block.block.shape}` + b.substring(blockIndexOnBoard + 1);
+      return b.substring(0, blockIndexOnBoard) + `${block.shape}` + b.substring(blockIndexOnBoard + 1);
     }, emptyBoard);
   }
 }
