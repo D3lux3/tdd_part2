414444639c36abaaa2f83aa8553c2f9084ed18f1
D3lux3 <dani.haapaniemi@hotmail.com>
2024-03-11 23:37:34 +0200

    Added basis for Shape object

---
+++ src/Shape.ts
@@ -0,0 +1,12 @@
+
+export class Shape {
+    static readonly T_SHAPE = {
+        symbol: "T",
+        orientations: {
+            0: [{ x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }, { x: 1, y: 2 }], // Facing down
+            1: [{ x: 0, y: 1 }, { x: 1, y: 1 }, { x: 1, y: 0 }, { x: 1, y: 2 }], // Facing left
+            2: [{ x: 1, y: 1 }, { x: 0, y: 2 }, { x: 1, y: 2 }, { x: 2, y: 2 }], // Facing up
+            3: [{ x: 1, y: 0 }, { x: 1, y: 1 }, { x: 1, y: 2 }, { x: 2, y: 1 }], // Facing right
+        }
+    }
+}
