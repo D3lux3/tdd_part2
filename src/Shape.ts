f745e005b8daedbbb3e94b73fe3493cda8650704
D3lux3 <dani.haapaniemi@hotmail.com>
2024-03-11 23:41:37 +0200

    Added I shape to Shape class

--- src/Shape.ts
+++ src/Shape.ts
@@ -9,4 +9,11 @@ export class Shape {
             3: [{ x: 1, y: 0 }, { x: 1, y: 1 }, { x: 1, y: 2 }, { x: 2, y: 1 }], // Facing right
         }
     }
+    static readonly I_SHAPE = {
+        symbol: "I",
+        orientations: {
+            0: [{ x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }, { x: 3, y: 1 }], // Facing down
+            1: [{ x: 2, y: 0 }, { x: 2, y: 1 }, { x: 2, y: 2 }, { x: 2, y: 3 }], // Facing left
+        }
+    }
 }
