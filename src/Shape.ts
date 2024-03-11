1f2a541838ab1c6100c5848172b9daa45732f9c9
D3lux3 <dani.haapaniemi@hotmail.com>
2024-03-12 01:24:59 +0200

    Temp added old coordinates to O shape

--- src/Shape.ts
+++ src/Shape.ts
@@ -30,7 +30,7 @@ export class Shape {
     static readonly O_SHAPE = {
         symbol: "O",
         orientations: {
-            0: [{ x: 1, y: 1 }, { x: 2, y: 1 }, { x: 1, y: 2 }, { x: 2, y: 2 }]
+            0: [{ x: 1, y: 0 }, { x: 2, y: 0 }, { x: 1, y: 1 }, { x: 2, y: 1 }] // Changed this to old Y
         }
     }
 }
