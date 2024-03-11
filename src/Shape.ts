86c4b420f750c7f90c908eddf5cebf268f3cf99d
D3lux3 <dani.haapaniemi@hotmail.com>
2024-03-11 23:44:03 +0200

    Added O shape

--- src/Shape.ts
+++ src/Shape.ts
@@ -12,8 +12,14 @@ export class Shape {
     static readonly I_SHAPE = {
         symbol: "I",
         orientations: {
-            0: [{ x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }, { x: 3, y: 1 }], // Facing down
-            1: [{ x: 2, y: 0 }, { x: 2, y: 1 }, { x: 2, y: 2 }, { x: 2, y: 3 }], // Facing left
+            0: [{ x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }, { x: 3, y: 1 }], // Horizontal
+            1: [{ x: 2, y: 0 }, { x: 2, y: 1 }, { x: 2, y: 2 }, { x: 2, y: 3 }], // Vertical
+        }
+    }
+    static readonly O_SHAPE = {
+        symbol: "O",
+        orientations: {
+            0: [{ x: 1, y: 1 }, { x: 2, y: 1 }, { x: 1, y: 2 }, { x: 2, y: 2 }]
         }
     }
 }
