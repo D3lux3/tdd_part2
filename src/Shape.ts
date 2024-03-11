81b5ffd4a895b8c8a0ff84b544ce1fa2c2d5301b
D3lux3 <dani.haapaniemi@hotmail.com>
2024-03-12 00:08:19 +0200

    added old orientations

--- src/Shape.ts
+++ src/Shape.ts
@@ -2,7 +2,14 @@
 export class Shape {
     static readonly T_SHAPE = {
         symbol: "T",
-        orientations: {
+        old_orientations: {
+            0: [{ x: 1, y: 0 }, { x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }], // Facing up
+            1: [{ x: 1, y: 0 }, { x: 1, y: 1 }, { x: 1, y: 2 }, { x: 2, y: 1 }], // Facing right
+            2: [{ x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }, { x: 1, y: 2 }], // Facing down
+            3: [{ x: 0, y: 1 }, { x: 1, y: 1 }, { x: 1, y: 0 }, { x: 1, y: 2 }], // Facing left
+
+        },
+        new_orientations: {
             0: [{ x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }, { x: 1, y: 2 }], // Facing down
             1: [{ x: 0, y: 1 }, { x: 1, y: 1 }, { x: 1, y: 0 }, { x: 1, y: 2 }], // Facing left
             2: [{ x: 1, y: 1 }, { x: 0, y: 2 }, { x: 1, y: 2 }, { x: 2, y: 2 }], // Facing up
