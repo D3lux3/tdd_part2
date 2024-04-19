a30a2d6c4c7ab6a5c68ea146a69ae71fefa00db4
D3lux3 <dani.haapaniemi@hotmail.com>
2024-04-19 18:45:10 +0200

    tetromino distribution test

--- test/Shufflebag.test.mjs
+++ test/Shufflebag.test.mjs
@@ -1,6 +1,7 @@
 import { beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
 import { Shufflebag } from "../src/Shufflebag.ts";
+import { Tetromino } from "../src/Tetromino.ts";
 
 describe("Shufflebag", () => {
     let shufflebag
@@ -10,5 +11,16 @@ describe("Shufflebag", () => {
 
     test("contains six tetrominos initially", () => {
         expect(shufflebag.getTetrominos().length).equal(6);
+    })
+
+    test("contains three T shape two O shape and one I shape tetrominos initially", () => {
+        const tetrominos = shufflebag.getTetrominos();
+        const t_shapes = tetrominos.filter((tetromino) => tetromino === Tetromino.T_SHAPE).length;
+        const o_shapes = tetrominos.filter((tetromino) => tetromino === Tetromino.O_SHAPE).length;
+        const i_shapes = tetrominos.filter((tetromino) => tetromino === Tetromino.I_SHAPE).length;
+
+        expect(t_shapes).equal(3);
+        expect(o_shapes).equal(2);
+        expect(i_shapes).equal(1);
     })
 })
