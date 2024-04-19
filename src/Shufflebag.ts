78307328e09a52a94d41f7e0587a400bdb5a1c37
D3lux3 <dani.haapaniemi@hotmail.com>
2024-04-19 19:29:04 +0200

    Pass id change test

--- src/Shufflebag.ts
+++ src/Shufflebag.ts
@@ -1,4 +1,5 @@
 import { Tetromino } from "./Tetromino"
+import { v4 as uuid } from 'uuid';
 export class Shufflebag {
 
     static readonly T_SHAPE_COUNT = 2;
@@ -7,19 +8,30 @@ export class Shufflebag {
 
     bagSize: number;
     bag: Tetromino[];
+    id: string;
 
-    constructor(bag?: Tetromino[], bagSize?: number) {
+    constructor(id?: string, bag?: Tetromino[], bagSize?: number) {
         this.bagSize = bagSize || 10;
-        this.bag = bag || this.generateTetrominos();
+        this.id = id || "";
+        this.bag =
+            bag
+                ? bag.length > 0 ? bag : this.generateTetrominos()
+                : this.generateTetrominos(id);
     }
 
-    private generateTetrominos() {
+    private generateTetrominos(id?: string) {
         const t_shapes = Math.floor(this.bagSize / Shufflebag.T_SHAPE_COUNT)
         const o_shapes = Math.floor(this.bagSize / Shufflebag.O_SHAPE_COUNT)
         const i_shapes = Math.floor(this.bagSize / Shufflebag.I_SHAPE_COUNT)
+        this.id = id || uuid();
         return [...Array(t_shapes).fill(Tetromino.T_SHAPE), ...Array(o_shapes).fill(Tetromino.O_SHAPE), ...Array(i_shapes).fill(Tetromino.I_SHAPE)].map(value => ({ value, sort: Math.random() }))
             .sort((a, b) => a.sort - b.sort)
-            .map(({ value }) => value)
+            .map(({ value }) => value);;
+    }
+
+    pull() {
+        const popped = this.bag.pop();
+        return [popped, new Shufflebag(this.id, this.bag, this.bagSize)]
     }
 
     getTetrominos() {
