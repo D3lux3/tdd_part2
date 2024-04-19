b8c8095ca80bb0bbf737ced04ffb37191dc4d917
D3lux3 <dani.haapaniemi@hotmail.com>
2024-04-19 18:37:03 +0200

    Added tests for shufflebag

---
+++ test/Shufflebag.test.mjs
@@ -0,0 +1,14 @@
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Shufflebag } from "../src/Shufflebag.js";
+
+describe("Shufflebag", () => {
+    let Shufflebag
+    beforeEach(() => {
+        Shufflebag = new Shufflebag(6);
+    });
+
+    test("contains six tetrominos initially", () => {
+        expect(subscriber.getPoints()).equal(0);
+    })
+})
