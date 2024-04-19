b30bc558c898e89559d4b6c0b089b2cab1794013
D3lux3 <dani.haapaniemi@hotmail.com>
2024-04-19 18:56:41 +0200

    Generates tetrominos with distributions

--- src/Shufflebag.ts
+++ src/Shufflebag.ts
@@ -1,14 +1,26 @@
 import { Tetromino } from "./Tetromino"
 export class Shufflebag {
+
+    static readonly T_SHAPE_COUNT = 2;
+    static readonly O_SHAPE_COUNT = 3;
+    static readonly I_SHAPE_COUNT = 5;
+
     bagSize: number;
     bag: Tetromino[];
 
-    constructor(bag: Tetromino[], bagSize?: number) {
+    constructor(bag?: Tetromino[], bagSize?: number) {
         this.bagSize = bagSize || 10;
-        this.bag = bag;
+        this.bag = bag || this.generateTetrominos();
+    }
+
+    private generateTetrominos() {
+        const t_shapes = Math.floor(this.bagSize / Shufflebag.T_SHAPE_COUNT)
+        const o_shapes = Math.floor(this.bagSize / Shufflebag.O_SHAPE_COUNT)
+        const i_shapes = Math.floor(this.bagSize / Shufflebag.I_SHAPE_COUNT)
+        return [...Array(t_shapes).fill(Tetromino.T_SHAPE), ...Array(o_shapes).fill(Tetromino.O_SHAPE), ...Array(i_shapes).fill(Tetromino.I_SHAPE)]
     }
 
     getTetrominos() {
-        return [Tetromino.T_SHAPE, Tetromino.T_SHAPE, Tetromino.T_SHAPE, Tetromino.O_SHAPE, Tetromino.O_SHAPE, Tetromino.I_SHAPE]
+        return this.bag
     }
 }
